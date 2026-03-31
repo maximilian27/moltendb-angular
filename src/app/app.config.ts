bd90c292bbd5978c6ea08f2916d834494608e54b
maximilian27 <maximilian.both27@outlook.com>
2026-03-31 19:39:06 +0300

    chore: use stable moltendb version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideMoltenDB} from "@moltendb-web/angular";
+import {provideMoltenDb} from "@moltendb-web/angular";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideMoltenDB({
+    provideMoltenDb({
       name: 'local_test_db',
       workerUrl: '/moltendb-worker.js'
     })
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
-import { Component, inject } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { moltenDbResource, MoltenService } from "@moltendb-web/angular";
+import {moltendbClient, moltenDbResource} from "@moltendb-web/angular";
 
 interface UserDoc {
   _key: string;
@@ -15,37 +15,39 @@ interface UserDoc {
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  private molten = inject(MoltenService);
-
+export class App implements OnInit{
+  private client = moltendbClient();
   /**
    * Now using the 'moltenDbResource' pattern.
    * This provides: .value(), .isLoading() and .error()
    */
-  users = moltenDbResource<UserDoc[]>('users', async (client) => {
+  users = moltenDbResource<UserDoc[]>('users', async (collection) => {
     console.log('Fetching users...');
-    // We expect an array of users
-    const result = await client.collection('users').get().exec();
+
+    const result = await collection.get().exec();
+
     return result as unknown as UserDoc[];
   });
-
   async addUser() {
     const randomId = Math.random().toString(36).substring(2, 9);
 
     // Using the cleaner .insert() if your query client supports it,
     // or staying with .set()
-    await this.molten.client.collection('users').set({
-      [randomId]: {
-        name: 'Angular Dev ' + randomId,
-        role: 'Admin'
-      }
-    }).exec();
+    await this.client.collection('users').set([{
+      name: 'Angular Dev ' ,
+      role: 'Admin'
+    }]).exec();
 
     console.log('Inserted user:', randomId);
   }
 
   async clearUsers() {
     // Just for testing the reactivity!
-    await this.molten.client.collection('users').delete().drop().exec();
+    await this.client.collection('users').delete().drop().exec();
+  }
+
+  public async ngOnInit(): Promise<void> {
+    const users = await this.client.collection('users').get().exec();
+    console.debug('App initialized', users);
   }
 }
